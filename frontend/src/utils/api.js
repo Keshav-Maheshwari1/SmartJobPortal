0b949b59812c6f1864bd1cd178f91f1a55317542
Keshav-Maheshwari <maheshwarikeshav029@gmail.com>
2025-06-26 06:05:06 +0530

    changes in backend url in frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://smart-job-portal-iota.vercel.app",
 });
 
 export const getToken = (roomName, userName) =>
